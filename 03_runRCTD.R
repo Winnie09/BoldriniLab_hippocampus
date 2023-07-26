3606aae7ee8e045fdd08f57ed76fd780b3ada510
whou10 <wp.hou3@gmail.com>
2023-07-26 16:15:43 -0400

    update path

--- 03_runRCTD.R
+++ 03_runRCTD.R
@@ -1,12 +1,12 @@
 library(Matrix)
 library(spacexr)
 library(Seurat)
-sampcor <- read.csv('/home/whou10/scratch16/whou10/boldrinin/doc/visium_snmultiome_sample.csv',as.is = T)
+sampcor <- read.csv('/home/whou10/scratch16/whou10/boldrini/doc/visium_snmultiome_sample.csv',as.is = T)
 
 jid <- as.numeric(commandArgs(trailingOnly = T))
   print(jid)
-  mat <- readRDS(paste0('/home/whou10/scratch16/whou10/boldrinin/visium/deconvolute/data/',tolower(sampcor[jid,2]),'/count.rds'))
-  ct <- readRDS(paste0('/home/whou10/scratch16/whou10/boldrinin/visium/deconvolute/data/',tolower(sampcor[jid,2]),'/celltype.rds'))
+  mat <- readRDS(paste0('/home/whou10/scratch16/whou10/boldrini/visium/deconvolute/data/',tolower(sampcor[jid,2]),'/count.rds'))
+  ct <- readRDS(paste0('/home/whou10/scratch16/whou10/boldrini/visium/deconvolute/data/',tolower(sampcor[jid,2]),'/celltype.rds'))
   tab <- table(ct)
   id <- which(ct %in% names(tab)[tab >= 25])
   ct <- ct[id]
@@ -17,7 +17,7 @@ jid <- as.numeric(commandArgs(trailingOnly = T))
   
   reference <- Reference(mat, ct, colSums(mat))
   
-  spa <- Load10X_Spatial(paste0('/home/whou10/scratch16/whou10/boldrinin/visium/data/',sampcor[jid,1]))
+  spa <- Load10X_Spatial(paste0('/home/whou10/scratch16/whou10/boldrini/visium/data/',sampcor[jid,1]))
   mat <- spa@assays$Spatial@counts
   coord <- spa@images$slice1@coordinates[,c('imagerow','imagecol')]
   puck <- SpatialRNA(coord, mat, colSums(mat))
@@ -27,6 +27,6 @@ jid <- as.numeric(commandArgs(trailingOnly = T))
   
   results <- myRCTD@results
   norm_weights = normalize_weights(results$weights) 
-  saveRDS(norm_weights,file=paste0('/home/whou10/scratch16/whou10/boldrinin/visium/deconvolute/res/',sampcor[jid,1],'.rds'))
+  saveRDS(norm_weights,file=paste0('/home/whou10/scratch16/whou10/boldrini/visium/deconvolute/res/',sampcor[jid,1],'.rds'))
 
 
